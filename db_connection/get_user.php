8b2c427dbac0a357ae4a05c08fd2259c97491bb6
AlexWorldD <malutin.alex@gmail.com>
2015-12-13 18:20:33 +0300

    Change load and edit user's js. Stable work.

--- db_connection/get_user.php
+++ db_connection/get_user.php
@@ -8,9 +8,7 @@
 include_once 'bd_connect_secure.php';
 if (isset($_POST['user_id'])){
     $user_id = $_POST['user_id'];
-    $request="SELECT first_name, last_name, sex, age, phone, email, country.country_id, country, city.city_id, city, address, address2
-FROM user join address join city join country
-WHERE user_id=".$user_id." and user.address_id=address.address_id and address.city_id=city.city_id and city.country_id=country.country_id;";
+    $request="SELECT first_name, last_name, sex, age, phone, email, address_id FROM user WHERE user_id='$user_id'";
     $data = mysqli_query($mysqli, $request);
     $items = array();
     while ($row = mysqli_fetch_object($data)) {
@@ -20,5 +18,19 @@ WHERE user_id=".$user_id." and user.address_id=address.address_id and address.ci
 }
 else {
 
-    echo 'Invalid request!';
+    if (isset($_POST['address_id'])){
+        $address_id = $_POST['address_id'];
+        $request="SELECT country.country_id, country, city.city_id, city, address, address2 FROM address join country join city WHERE
+address_id='$address_id' and address.city_id=city.city_id and city.country_id=country.country_id";
+        $data = mysqli_query($mysqli, $request);
+        $items = array();
+        while ($row = mysqli_fetch_object($data)) {
+            array_push($items, $row);
+        }
+        echo json_encode($items);
+    }
+    else {
+
+        echo 'Invalid request!';
+    }
 }
--- js/load_user.js
+++ js/load_user.js
@@ -1,35 +1,50 @@
 /**
  * Created by Алексей on 12.12.2015.
  */
-function load_user(it)
-{
+function load_user(it) {
     $.ajax({
-            type: "POST",
-            url: "http://localhost:8080/bd/db_connection/get_user.php",
-            dataType: "json",
-            data: "user_id="+it,
-            error: function () {
-                alert("Error with AJAX!");
-            },
-            success: function (data) {
+        type: "POST",
+        url: "http://localhost:8080/bd/db_connection/get_user.php",
+        dataType: "json",
+        data: "user_id=" + it,
+        error: function () {
+            alert("Error with AJAX!");
+        },
+        success: function (data) {
 
-                $('#f_name').val(data[0].first_name);
-                $('#l_name').val(data[0].last_name);
-                $('#age').val(data[0].age);
-                $('#sex').val(data[0].sex);
-                $('#email').val(data[0].email);
-                $('#phone').val(data[0].phone);
-                $('#country').append(
-                "<option value='"+data[0].country_id+"'>"+data[0].country+"</option>"
-                );
-                $('#country').val(data[0].country_id);
-                $('#city').append(
-                    "<option value='"+data[0].city_id+"'>"+data[0].city+"</option>"
-                );
-                $('#city').val(data[0].city_id);
-                $('#address').val(data[0].address);
-                $('#address2').val(data[0].address2);
+            $('#f_name').val(data[0].first_name);
+            $('#l_name').val(data[0].last_name);
+            $('#age').val(data[0].age);
+            $('#sex').val(data[0].sex);
+            $('#email').val(data[0].email);
+            $('#phone').val(data[0].phone);
+            // Check address
+            if (data[0].address_id) {
+                $.ajax({
+                    type: "POST",
+                    url: "http://localhost:8080/bd/db_connection/get_user.php",
+                    dataType: "json",
+                    data: "address_id=" + data[0].address_id,
+                    error: function () {
+                        alert("Error with second AJAX!");
+                    },
+                    success: function (data) {
+                        $('#country').append(
+                            "<option value='" + data[0].country_id + "'>" + data[0].country + "</option>"
+                        );
+                        $('#country').val(data[0].country_id);
+                        $('#city').append(
+                            "<option value='" + data[0].city_id + "'>" + data[0].city + "</option>"
+                        );
+                        $('#city').val(data[0].city_id);
+                        $('#address').val(data[0].address);
+                        $('#address2').val(data[0].address2);
+
+                    }
+                })
             }
+
         }
-    )
+    })
 }
+
