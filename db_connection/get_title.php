c836d2b93de6e935ac2bcba25fdf8864ae3eb637
AlexWorldD <malutin.alex@gmail.com>
2015-12-15 04:11:59 +0300

    Value and data for autocomplete.

--- db_connection/get_title.php
+++ db_connection/get_title.php
@@ -10,7 +10,7 @@ include_once "bd_connect_secure.php";
 if (isset($_GET['query'])) {
     $str = $_GET['query'];
     // Prepare the statement for our search.
-    $request = $mysqli->prepare("SELECT title as res FROM writing WHERE title LIKE ? ORDER BY res");
+    $request = $mysqli->prepare("SELECT title as `value`, writing_id as data FROM writing WHERE title LIKE ? ORDER BY `value`");
     $str1 = $str . '%';
     $request->bind_param('s', $str1);
     $res = $request->execute();
@@ -21,7 +21,7 @@ if (isset($_GET['query'])) {
 
     $res = $request->get_result();
 
-    $sugestions = $res->fetch_all();
-    $re = array('query' => $str, 'suggestions' => $sugestions[0]);
+    $sugestions = $res->fetch_all(MYSQLI_ASSOC);
+    $re = array('query' => $str, 'suggestions' => $sugestions);
     echo json_encode($re);
 }
