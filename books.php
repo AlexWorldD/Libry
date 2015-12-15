50f286e89038c3124308d055701d2c65e01eba79
AlexWorldD <malutin.alex@gmail.com>
2015-12-16 01:38:48 +0300

    Add login checking for ALL pages. Main rename to index.

--- books.php
+++ books.php
@@ -5,10 +5,13 @@
  * Date: 11.12.2015
  * Time: 21:30
  */
-include_once 'db_connection/bd_connect_secure.php';
+include_once 'db_connection/bd_connect.php';
 include_once 'db_connection/functions.php';
 sec_session_start(); // start secure session
 ?>
+<?php if (login_check($mysqli) == true) :
+mysqli_close($mysqli);
+include_once 'db_connection/bd_connect_secure.php';?>
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/html">
 <head>
@@ -55,7 +58,7 @@ sec_session_start(); // start secure session
                 </div>
             </li>
             <li>
-                <a href="main.php">Main</a>
+                <a href="index.php">Main</a>
             </li>
             <li>
                 <a href="mybooks.php">My books</a>
@@ -170,3 +173,59 @@ sec_session_start(); // start secure session
 </div>
 </body>
 </html>
+<?php else : ?>
+    <?php
+    include_once 'db_connection/bd_connect.php';
+    if (login_check($mysqli) == true) {
+        $logged = 'in';
+    } else {
+        $logged = 'out';
+    }
+    ?>
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Sign In</title>
+        <link rel="stylesheet" href="styles/main.css" />
+        <meta charset="UTF-8">
+        <title>Login page</title>
+        <link href="Bootstrap3/dist/css/bootstrap.min.css" rel="stylesheet" media="screen">
+        <link href="css/login_style.css" rel="stylesheet" media="screen">
+        <script src="http://code.jquery.com/jquery-latest.js"></script>
+        <script src="Bootstrap3/dist/js/bootstrap.min.js"></script>
+        <script type="text/JavaScript" src="js/sha512.js"></script>
+        <script type="text/JavaScript" src="js/hash_functions.js"></script>
+    </head>
+    <body>
+    <?php
+    if (isset($_GET['error'])) {
+        echo '<p class="error">Error Logging In!</p>';
+    }
+    ?>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-6 col-md-4 col-md-offset-4">
+                <h1 class="text-center login-title">Sign In</h1>
+                <div class="account-wall">
+                    <?php
+                    if (!empty($er_msg)) {
+                        echo '<h4 class="text-center warning">'.$er_msg.'</h4>';
+                    }
+                    ?>
+                    <form class="form-signin" action="db_connection/login_process.php" method="post" name="login_form">
+                        <input type="text" class="form-control" placeholder="Username" required autofocus name="username">
+                        <input type="password" class="form-control" placeholder="Password" required name="password">
+                        <input type="button" class="btn btn-lg btn-primary btn-block"
+                               value="Login"
+                               onclick="formhash(this.form, this.form.password);" />
+                        <a href="#" class="pull-right need-help">Need help? </a><span class="clearfix"></span>
+                    </form>
+                </div>
+                <a href="register2.php" class="text-center new-account">Register </a>
+            </div>
+        </div>
+    </div>
+    </body>
+    </html>
+<?php endif; ?>
