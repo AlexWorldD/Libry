44af4775e867b080c91cfb78043380a291ed3183
AlexWorldD <malutin.alex@gmail.com>
2015-12-16 12:46:45 +0300

    Add EER for Libry.

---
+++ SQL/tmp.sql
@@ -0,0 +1,7 @@
+update user set first_name='A',
+last_name="Малютин",
+age=21,
+sex='M',
+email='boss@me.com',
+phone='+79990000007' where user_id=1;
+INSERT INTO address (address, address2, city_id) VALUES ('Ботаническая, 64/9', '' , 1);
